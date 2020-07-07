0aaed5b3cf23ba81323b12f2a3d11c4a864c6729
Harsh Sharma <sharma.harsh7111@gmail.com>
2020-07-06 22:34:59 -0600

    add nav bar

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bulma/css/bulma.css';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
+  
 );
 
 // If you want your app to work offline and load faster, you can change
